b26c2b034550f65c84e84aedf0a7060fb4377785
Jake Obodomechine <jaketuriacada@gmail.com>
2025-04-18 20:29:39 +0100

    Room clean

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        chunkSizeWarningLimit: 1000, // in KB
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
